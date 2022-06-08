7ff655082a6b960dab72159def43b8422d884982
Brett Treweek <bretttrew@gmail.com>
2022-06-08 11:20:40 +0800

    changed to deploy urls

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -83,7 +83,7 @@ const AppProvider = ({ children }) => {
 	// Axios instance
 	// https://round-report-v2.herokuapp.com
 	const authFetch = axios.create({
-		baseURL: '/api/v1',
+		baseURL: 'https://round-report-v2.herokuapp.com/api/v1',
 	});
 
 	// Axios request interceptor
@@ -145,7 +145,7 @@ const AppProvider = ({ children }) => {
 		dispatch({ type: REGISTER_USER_BEGIN });
 		try {
 			const response = await axios.post(
-				'/api/v1/auth/register',
+				'https://round-report-v2.herokuapp.com/api/v1/auth/register',
 				currentUser
 			);
 			console.log('response', response);
@@ -169,7 +169,7 @@ const AppProvider = ({ children }) => {
 		dispatch({ type: LOGIN_USER_BEGIN });
 		try {
 			const { data } = await axios.post(
-				'/api/v1/auth/login',
+				'https://round-report-v2.herokuapp.com/api/v1/auth/login',
 				currentUser
 			);
 			console.log('response', data);
